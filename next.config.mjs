91f434d5c54ada758ea39b1cf00de545c51dd2ac
BryanViews002 <bryanjoe0012@gmail.com>
2026-05-02 10:51:29 -0700

    third commit2

--- next.config.mjs
+++ next.config.mjs
@@ -26,6 +26,12 @@ const nextConfig = {
       allowedOrigins: ['localhost:3000'],
     },
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
